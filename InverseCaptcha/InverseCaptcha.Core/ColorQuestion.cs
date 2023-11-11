bc84a65165f5c1d64632c2d4bb4b04b0af8374a9
Andreas Knudsen <andreas.knudsen@gmail.com>
2023-11-11 10:38:20 +0100

    temp

---
+++ InverseCaptcha/InverseCaptcha.Core/ColorQuestion.cs
@@ -0,0 +1,34 @@
+﻿namespace InverseCaptcha.Core;
+
+public class ColorQuestion: Question
+{
+    string[] rainbow = new[] { "red", "orange", "yellow", "green", "blue", "indigo", "violet" };
+    string[] colors = new[] { "Purple", "Black", "White", "Pink", "Gray", "Brown", "Cyan", "Magenta" };
+
+    public ColorQuestion()
+    {
+        Regen();
+    }
+
+    public override void RegenerateQuestion()
+    {
+        Regen();
+    }
+
+    private void Regen()
+    {
+        var rand = new Random();
+        var rainbowColor = rainbow[rand.Next(rainbow.Length)];
+        QuestionText = $"What is a color in the rainbow next to the color {rainbowColor}";
+        var previousColor = rainbow[(Array.IndexOf(rainbow, rainbowColor) - 1 + rainbow.Length) % rainbow.Length];
+        var nextColor = rainbow[(Array.IndexOf(rainbow, rainbowColor) + 1) % rainbow.Length];
+        
+        HumanAnswers = new List<string> { previousColor, nextColor };
+        AnswerCategories = new List<AnswerCategory>
+        {
+            new("OtherColors", colors, null),
+            new("RainbowColors", rainbow, null),
+            new("Hex", new[]{""},new[]{"\\d{6}","#\\d{6}"})
+        };
+    }
+}
--- InverseCaptcha/InverseCaptcha.Core/CountryQuestion.cs
+++ InverseCaptcha/InverseCaptcha.Core/CountryQuestion.cs
@@ -6,18 +6,33 @@ public class CountryQuestion : Question
 {
     private readonly List<Country> _countries;
     private readonly List<Continent> _continents;
+    private AnswerCategory? otherInContinent = null;
+    private AnswerCategory? otherInOtherContinents;
 
     public CountryQuestion() : base()
     {
         _countries = ReadCountries();
         _continents = CreateContinents();
 
-        var chosenCountry = _countries.First();
+        Regen();
+
+    }
+
+    private void Regen()
+    {
+        var random = new Random();
+        var chosenCountry = _countries[random.Next(_countries.Count)];
         QuestionText = $"Name a neighbouring country to {chosenCountry.Name}";
         AnswerCategories = GenerateAnswerCategories(chosenCountry);
-        RequiredCategoriesToPass = AnswerCategories.Count;
         HumanAnswers = _countries.Where(c => chosenCountry.NeighbourCodes.Contains(c.Code)).Select(c => c.Name).ToList();
     }
+
+
+    public override void RegenerateQuestion()
+    {
+        Regen();
+    }
+
 
     private List<Country> ReadCountries()
     {
@@ -31,24 +46,32 @@ public class CountryQuestion : Question
     {
         return new List<Continent>()
         {
-            new Continent("Africa", "AF", _countries.Where(c => c.Continent == "AF").ToList()),
-            new Continent("Europe", "EU", _countries.Where(c => c.Continent == "EU").ToList()),
-            new Continent("Asia", "AS", _countries.Where(c => c.Continent == "AS").ToList()),
-            new Continent("North America", "NA", _countries.Where(c => c.Continent == "NA").ToList()),
-            new Continent("South America", "SA", _countries.Where(c => c.Continent == "SA").ToList()),
-            new Continent("Oceania", "OC", _countries.Where(c => c.Continent == "OC").ToList()),
-            new Continent("Antarctica", "AN", _countries.Where(c => c.Continent == "AN").ToList()),
+            new ("Africa", "AF", _countries.Where(c => c.Continent == "AF").ToList()),
+            new ("Europe", "EU", _countries.Where(c => c.Continent == "EU").ToList()),
+            new ("Asia", "AS", _countries.Where(c => c.Continent == "AS").ToList()),
+            new ("North America", "NA", _countries.Where(c => c.Continent == "NA").ToList()),
+            new ("South America", "SA", _countries.Where(c => c.Continent == "SA").ToList()),
+            new ("Oceania", "OC", _countries.Where(c => c.Continent == "OC").ToList()),
+            new ("Antarctica", "AN", _countries.Where(c => c.Continent == "AN").ToList()),
         };
     }
 
     private List<AnswerCategory> GenerateAnswerCategories(Country country)
     {
         var continentForCountry = _continents.Single(f => f.Code == country.Continent);
-        var otherInContinent = new AnswerCategory("Other countries in continent", continentForCountry.Countries.Where(c => !country.NeighbourCodes.Contains(c.Code)).Select(c => c.Name).ToArray());
+        otherInContinent ??= new AnswerCategory("Other countries in continent", Array.Empty<string>());
+        otherInContinent.Answers = continentForCountry.Countries.Where(c => !country.NeighbourCodes.Contains(c.Code))
+            .Select(c => c.Name).ToArray();
+        
+        otherInOtherContinents ??= new AnswerCategory("Other countries in other continents", Array.Empty<string>());
+        otherInContinent.Answers = _continents.Where(x=>x.Code != country.Continent).SelectMany(x=>x.Countries.Where(c => !country.NeighbourCodes.Contains(c.Code)))
+            .Select(c => c.Name).ToArray();
         return new List<AnswerCategory>
         {
             otherInContinent, 
+            otherInOtherContinents, 
             new ("Garbage", new string[0], new []{"\\w+"}),
+            new ("Blank", new string[0], new []{""}),
         };
     }
 }
